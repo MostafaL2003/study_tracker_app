696fb6adcd5866b5054ee5fbf66e6a4fd90ec515
Mostafa mamdouh <MostafaW2003@outlook.com>
2025-07-03 21:36:07 +0300

    Initialize Rest of the pages

---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: CupertinoColors.white,
+        body:Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset("assets/image/logo.png",height: 150,color: Colors.black,),
+            Center(child: Text("Study Tracker", style: TextStyle(fontFamily: "Oswald",fontSize: 60),)),
+            Text("Track. Study. Succeed.", style: TextStyle(fontSize: 20, fontFamily: "Oswald"),),
+            SizedBox(height: 300,),
+            ElevatedButton(onPressed: (){Navigator.pushNamed(context, '/HomeScreen');}, child: Text("Get Started",style: TextStyle(fontSize: 30 ,color: Colors.black ,), ))
+            
+        ],) ,
+      ),
+      
+    );
+  }
+}
