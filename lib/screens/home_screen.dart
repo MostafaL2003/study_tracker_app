00e549e3a6a1c31513c9d30f879cb73d024f51ff
Mostafa mamdouh <MostafaW2003@outlook.com>
2025-07-06 17:44:47 +0300

    Finish General Functionality of the app

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:study_tracker_app/util/track_tile.dart';
 
@@ -11,17 +12,47 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   List subjectList = [
     //(subjectName ,  timerStart , timeSpent  ,timeGoal)
-    ["Math", false, 30, 60],
+    ["Math", false, 50, 1],
     ["Science", true, 30, 60],
     ["English", false, 10, 120],
   ];
   void timerStarted(int index) {
+    var startTime = DateTime.now();
+    //
+    int elapsedTime = subjectList[index][2];
+
     setState(() {
       subjectList[index][1] = !subjectList[index][1];
     });
+
+    if (subjectList[index][1]) {
+      Timer.periodic(Duration(seconds: 1), (timer) {
+        setState(() {
+          if (!subjectList[index][1]) {
+            timer.cancel();
+          }
+        });
+        setState(() {
+          var currentTime = DateTime.now();
+          subjectList[index][2] =
+              currentTime.second -
+              startTime.second +
+              60 * (currentTime.minute - startTime.minute) +
+              60 * 60 * (currentTime.hour - startTime.hour) +
+              elapsedTime;
+        });
+      });
+    }
   }
 
-  void settingsOpened(int index) {}
+  void settingsOpened(int index) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(title: Text("Settings"));
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
--- lib/util/track_tile.dart
+++ lib/util/track_tile.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:percent_indicator/percent_indicator.dart';
 
 class TrackTile extends StatelessWidget {
-
   final String subjectName;
   final VoidCallback onTap;
   final VoidCallback settingsTap;
@@ -10,44 +9,96 @@ class TrackTile extends StatelessWidget {
   final int timeGoal;
   final bool timerStart;
 
+  const TrackTile({
+    super.key,
+    required this.subjectName,
+    required this.onTap,
+    required this.settingsTap,
+    required this.timeSpent,
+    required this.timeGoal,
+    required this.timerStart,
+  });
 
-  const TrackTile({super.key,required this.subjectName, required this.onTap , required this.settingsTap, required this.timeSpent, required this.timeGoal, required this.timerStart});
-  
+  String minToSec(int totalSeconds) {
+    String secs = (totalSeconds % 60).toString();
+    String mins = (totalSeconds / 60).toStringAsFixed(4);
+    String hours = (totalSeconds / 60 / 60).toStringAsFixed(4);
+
+    if (secs.length == 1) {
+      secs = "0" + secs;
+    }
+    if (hours[1] == '.') {
+      hours = hours.substring(0, 1);
+    }
+    if (mins[1] == '.') {
+      mins = mins.substring(0, 1);
+    }
+
+    return "$hours:$mins:$secs";
+  }
+
+  double percentCompleted() {
+    return (timeSpent / (timeGoal * 60)).clamp(0.0, 1.0);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return  Padding(
-          padding: const EdgeInsets.only(top: 15, left: 10,right: 10),
-          child: Container(
-            padding: const EdgeInsets.all(20),
-            decoration: BoxDecoration(
-                color: Colors.grey[300],
-                borderRadius: BorderRadius.circular(15)
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children:[
-                Row(children: [
-                  GestureDetector(
-                    onTap: onTap,
-                child: CircularPercentIndicator(radius: 40, center:Icon(timerStart ? Icons.pause : Icons.play_arrow,size: 40,)),),
-                SizedBox(width: 15,),
+    return Padding(
+      padding: const EdgeInsets.only(top: 15, left: 10, right: 10),
+      child: Container(
+        padding: const EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          color: Colors.grey[300],
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                GestureDetector(
+                  onTap: onTap,
+                  child: CircularPercentIndicator(
+                    progressColor: Colors.green,
+                    percent: percentCompleted(),
+                    radius: 40,
+                    center: Icon(
+                      timerStart ? Icons.pause : Icons.play_arrow,
+                      size: 40,
+                    ),
+                  ),
+                ),
+                SizedBox(width: 15),
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Text(subjectName,style: TextStyle(fontSize: 30, fontFamily: "Oswald"),),
-                    Text( timeSpent.toString()+ '/' +timeGoal.toString(),
-                    style: TextStyle(color:Colors.grey[600] ,fontSize: 15, fontFamily: "Oswald"),),
+                    Text(
+                      subjectName,
+                      style: TextStyle(fontSize: 30, fontFamily: "Oswald"),
+                    ),
+                    Text(
+                      minToSec(timeSpent) +
+                          "-" +
+                          timeGoal.toString() +
+                          "-" +
+                          (percentCompleted() * 100).toStringAsFixed(0) +
+                          "%",
+                      style: TextStyle(
+                        color: Colors.grey[600],
+                        fontSize: 15,
+                        fontFamily: "Oswald",
+                      ),
+                    ),
                   ],
                 ),
-                ],),
-                
-                GestureDetector(
-                  onTap: settingsTap,
-                  child: Icon(Icons.settings))
               ],
             ),
-          ),
-        );
+
+            GestureDetector(onTap: settingsTap, child: Icon(Icons.settings)),
+          ],
+        ),
+      ),
+    );
   }
 }
